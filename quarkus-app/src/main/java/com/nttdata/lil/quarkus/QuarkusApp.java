bab65592e2839ef4dbc5a7fae0337b967dde194f
Robert Stoian <robert.stoian@nttdata.com>
2024-05-07 16:48:38 +0300

    use panache

--- quarkus-app/src/main/java/com/nttdata/lil/quarkus/QuarkusApp.java
+++ quarkus-app/src/main/java/com/nttdata/lil/quarkus/QuarkusApp.java
@@ -23,8 +23,10 @@ public class QuarkusApp implements QuarkusApplication {
 	@Override
 	@ActivateRequestContext // or run transaction
 	public int run(String... args) throws Exception {
-		List<Service> services = this.serviceRepository.getAllServices();
+		List<Service> services = this.serviceRepository.listAll();
 		services.forEach(System.out::println);
+		Service service = this.serviceRepository.findById(2L);
+		System.out.println(service);
 		return 0;
 	}
 }
--- quarkus-app/src/main/java/com/nttdata/lil/quarkus/data/repository/ServiceRepository.java
+++ quarkus-app/src/main/java/com/nttdata/lil/quarkus/data/repository/ServiceRepository.java
@@ -5,20 +5,21 @@ import java.util.List;
 
 import com.nttdata.lil.quarkus.data.entity.Service;
 
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.persistence.EntityManager;
 
 @ApplicationScoped
-public class ServiceRepository {
+public class ServiceRepository implements PanacheRepository<Service> {
 
-	private final EntityManager em;
-
-	public ServiceRepository(EntityManager em) {
-		this.em = em;
-	}
-
-	public List<Service> getAllServices(){
-		List<Service> services = this.em.createQuery("select service from Service service", Service.class).getResultList();
-		return services;
-	}
+//	private final EntityManager em;
+//
+//	public ServiceRepository(EntityManager em) {
+//		this.em = em;
+//	}
+//
+//	public List<Service> getAllServices(){
+//		List<Service> services = this.em.createQuery("select service from Service service", Service.class).getResultList();
+//		return services;
+//	}
 }
